ae5106e8fb8627ae6cfcb16067fdce5ef49d7a82
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-19 17:07:48 +0300

    Message Model

---
+++ src/messages/messageModel.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+
+import db from '../config/dbconfig';
+
+interface MessageAttributes {
+    id: Number;
+    topic: string;
+    message: string;
+    recipientNumber: string;
+    origin: string;
+}
+
+type MessageCreationAttributes = Optional<MessageAttributes, 'id'>;
+
+export class MessageModel extends Model<MessageAttributes, MessageCreationAttributes> {}
+
+MessageModel.init({
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    topic: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    recipientNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "recipient_number"
+    },
+    origin: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    sequelize: db,
+    tableName: "messages",
+    modelName: "Message"
+});
