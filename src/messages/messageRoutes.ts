bae5b3c16852af07e92230b415d951d266626f69
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-20 18:46:08 +0300

    added data validator and validation middleware to routes

--- src/messages/messageRoutes.ts
+++ src/messages/messageRoutes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
 import {
+    checkGetUsers,
+    checkSendManySms,
+    checkSendSingleSms
+} from './messageValidator';
+
+import {
     checkBalance,
     deliveryReport,
     getMessages,
@@ -8,17 +14,19 @@ import {
     sendOneSms
 } from './messageController';
 
+import { validationMiddleware } from './messageMiddleware';
+
 const router = express.Router();
 
-router.post('/send-one-sms', sendOneSms);
+router.post('/send-one-sms', checkSendSingleSms(), validationMiddleware, sendOneSms);
 
-router.post('/send-many-sms', sendManySms);
+router.post('/send-many-sms', checkSendManySms(), validationMiddleware, sendManySms);
 
 router.post('/delivery-reports', deliveryReport);
 
 router.get('/balance', checkBalance);
 
-router.get('/get-messages', getMessages);
+router.get('/get-messages', checkGetUsers(), validationMiddleware, getMessages);
 
 export default router;
 
